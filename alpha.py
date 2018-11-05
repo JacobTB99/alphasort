d6abc18533a1aa3916ff12d0042eafb40ebf80a3
JacobTB99 <jb467917@ohio.edu>
2018-11-05 17:58:37 -0500

    Commented out scaffolding messages

--- alpha.py
+++ alpha.py
@@ -16,15 +16,15 @@ def listscan(list):
         else:  #Base case. Once scan is through, exit
             print(list)
             return False
-            
+
 def shifter(list):
     """Moves to, and finds out-of-order pairs, and reorders
     Accepts a list as a parameter
     """
-    sc1 = "objects "  #Scaffolding message variables. Temporary
-    sc2 = " and "
-    sc3 = " switched"
-    sc4 = " in order"
+    #sc1 = "objects "  #Scaffolding message variables. Temporary
+    #sc2 = " and "
+    #sc3 = " switched"
+    #sc4 = " in order"
     n = len(list)  #Assign length of list to variable n
     x = 0  #Start at first position in list
     while listscan(list):
@@ -33,15 +33,15 @@ def shifter(list):
             t2 = list[x + 1]
             list[x + 1] = t1
             list[x] = t2
-            print(sc1 + str(x) + sc2 + str(x + 1) + sc3)
+            #print(sc1 + str(x) + sc2 + str(x + 1) + sc3)
             if x + 1 < n - 1:  #Only when not at end
                 x = x + 1 #Move position one more right
             else:  #Base case when unsorted
                 x = 0  #Restart Cycle
         else: #If sorted, and more room to right, move over one, leave items in position.
             if x + 1 < n - 1:
-                print(sc1 + str(x) + sc2 + str(x + 1) + sc4)  
-                x = x + 1  
+                #print(sc1 + str(x) + sc2 + str(x + 1) + sc4)
+                x = x + 1
             else:  #Base case. If at end of list, and items in order, leave.
                 print(sc1 + str(x) + sc2 + str(x + 1) + sc4)
                 x = 0  #Restart cycle
